1b678f107914b14305db293a892ab4a1bd114c69
Yrvin Escorihuela <yrvin.escorihuela>
2019-10-07 23:51:20 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Title } from './components/Title'
+import { SearchForm } from './components/SearchForm'
+import { Movie } from './components/Movie';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bulma/css/bulma.css';
+class App extends Component {
+  state  = { results: [] }
+  _handleResults = ( results ) => {
+    this.setState({results})
+  }
+  _renderResults () {
+    const { results } = this.state;
+    return results.map( movie => {
+      return <Movie
+                year={movie.Year}
+                poster={movie.Poster}
+                title={movie.Title}
+                key={movie.imdbID}/>
+    })
+  }
+  render() {
+    return (
+        <div className='App'>
+          <Title>Search movies</Title>
+          <div className='SearchForm-wrapper'>
+            <SearchForm onResults={this._handleResults}/>
+          </div>
+          <br/>
+          {this.state.results.length === 0
+            ? <p>Sin resultados</p>
+            : this._renderResults()
+          }
+        </div>
+    )
+  }
 }
-
 export default App;
